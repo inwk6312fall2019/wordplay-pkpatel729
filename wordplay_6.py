246028eb17e2af5dc9132434e49619ed4bbac821
pkpatel729 <pr346225@dal.ca>
2019-09-22 15:09:47 -0700

    calling the created function with passing arguments with every words of words.txt file.

--- wordplay_6.py
+++ wordplay_6.py
@@ -5,3 +5,14 @@ def is_abecedarian(word):
             return False
         previous_letter = letter
     return True
+
+fin = open('words.txt')
+count = 0
+
+for line in fin:
+    word = line.strip()
+    if is_abecedarian(word) == True:
+        print(word)
+        count += 1
+
+print(count)
