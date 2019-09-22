efad512e2d46760381a48636c16e8ad71911c66c
pkpatel729 <pr346225@dal.ca>
2019-09-22 15:26:09 -0700

    2nd commit as adding more lines of command for this program

--- wordplay_7.py
+++ wordplay_7.py
@@ -1,8 +1,8 @@
-def three_consecutive(s):
+def three_cons(str):
     i = 0
     count = 0
-    while i < len(s) - 1:
-        if s[i] == s[i + 1]:
+    while i < len(str) - 1:
+        if str[i] == str[i + 1]:
             count += 1
             if count == 3:
                 return True
@@ -11,3 +11,12 @@ def three_consecutive(s):
             count = 0
             i += 1
 
+
+fin = open('words.txt')
+count = 0
+for word in fin:
+    word = word.strip()
+    if three_consecutive(word) == True:
+        print(word)
+        count += 1
+print(count)
